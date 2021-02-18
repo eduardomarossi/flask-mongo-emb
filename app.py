61e2225448419872bcbd1f500f585add23333624
root <root@ip-172-31-74-55.ec2.internal>
2021-02-18 14:47:51 +0000

    1.0.1

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
+from decouple import config
 
 app = Flask(__name__)
 
+
+MONGO_HOST = config('MONGO_HOST')
+MONGO_USERNAME = config('MONGO_USERNAME')
+MONGO_PASSWORD = config('MONGO_PASSWORD')
+
 @app.route('/', methods=['POST'])
 def insert_student_progress():
-    REQUIRED_FIELDS = ['name', 'time', 'id', 'url']
+    REQUIRED_FIELDS = ['name', 'time', 'id', 'url', 'semester', 'course']
     for k in REQUIRED_FIELDS:
         if k not in request.form:
             return jsonify(status='ERROR', error='Missing field {}'.format(k))
 
-    client = MongoClient(host='###', username='###', password='####', authSource='###')
+    client = MongoClient(host=MONGO_HOST, username=MONGO_USERNAME, password=MONGO_PASSWORD, authSource='admin')
     db = client['test']
     collection = db['progress']
     collection.insert_one(request.form.to_dict())
